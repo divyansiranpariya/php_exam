0cb927c64b34a094761fe28ffe8b1233f64e8b9b
divyansiranpariya <anshiranpariya@gmail.com>
2024-10-17 15:53:34 +0530

    second

---
+++ student/api/v1/api_delete_student.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Access-Control-Allow-Methods:DELETE");
+header("Content-Type:application/json");
+include("../../config/config.php");
+
+$config=new Config();
+
+
+    if($_SERVER['REQUEST_METHOD']=='DELETE'){
+$input=file_get_contents('php://input');
+parse_str($input,$_DELETE);
+
+       $id=$_DELETE['id'];
+      $res= $config->deleteStudentDetail($id);
+      if($res){
+        $arr['data']="StudentDetails delete successfully..";
+      }else{
+          $arr['data']="StudentDetails delete failed..";
+      }
+
+}else{
+    $arr['error']="Only DELETE http request method is allowed...";
+}
+
+echo json_encode($arr);
+?>
